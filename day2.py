6037b3104d151abbeeabc96959db3d7b3a5a508d
Erlend Thorsrud <erlendthorsrud@gmail.com>
2021-12-02 20:33:46 +0100

    Solved part 1 for day 2

---
+++ day2.py
@@ -0,0 +1,18 @@
+#!/bin/bash/python3
+x = 0
+y = 0
+
+with open('data/day2-data.txt') as file: 
+    for line in file:
+        data = line.split(" ", 1)
+        command = data[0]
+        value = int(data[1])
+        match command: 
+            case 'up': 
+                y += value
+            case 'down':
+                y -= value
+            case 'forward':
+                x += value
+
+print(-1*x*y)
