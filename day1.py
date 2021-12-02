16ee1391afed7f6d3c26b0b7c78026574fba16c5
Erlend Thorsrud <erlendthorsrud@gmail.com>
2021-12-02 19:53:07 +0100

    Restructure

--- day1.py
+++ day1.py
@@ -2,21 +2,21 @@
 
 
 ### Part one
-# previous_measure = 0
-# increased_measure = 0
-# with open('data.txt') as file: 
-#     for line in file:
-#         if str(line) > str(previous_measure):
-#             increased_measure += 1
-#         previous_measure = line
+previous_measure = 0
+increased_measure = 0
+with open('data/day1-data.txt') as file: 
+    for line in file:
+        if str(line) > str(previous_measure):
+            increased_measure += 1
+        previous_measure = line
 
-# print(increased_measure)
+print(increased_measure)
 
 ### Part two
 queue = []
 previous_window = 0
 increased = 0
-with open('data.txt') as file:
+with open('data/day1-data.txt') as file:
     for line in file: 
         if len(queue) < 3:
             queue.append(int(line))
@@ -28,6 +28,7 @@ with open('data.txt') as file:
                 increased += 1
             print("previous " + str(previous_window))
             print("queue " + str(sum(queue)))
+            
 print("increased part two: " + str(increased))
         
 
