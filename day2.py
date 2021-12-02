0e1e5a385e5fa1a5fef248e3886740cbd662c143
Erlend Thorsrud <erlendthorsrud@gmail.com>
2021-12-02 21:13:52 +0100

    Refactored

--- day2.py
+++ day2.py
@@ -1,14 +1,12 @@
 #!/bin/bash/python3
 
 ### Part 1
-x = 0
-y = 0
+x = y = 0
 
 with open('data/day2-data.txt') as file: 
     for line in file:
-        data = line.split(" ", 1)
-        command = data[0]
-        value = int(data[1])
+        command, value = line.split()
+        value = int(value)
         match command: 
             case 'up': 
                 y -= value
@@ -21,15 +19,12 @@ print("Part 1: " + str(x*y))
 
 ### Part 2
 
-x = 0
-y = 0
-aim = 0
+x = y = aim = 0
 
 with open('data/day2-data.txt') as file: 
     for line in file:
-        data = line.split(" ", 1)
-        command = data[0]
-        value = int(data[1])
+        command, value = line.split()
+        value = int(value)
         match command: 
             case 'up': 
                 aim -= value
