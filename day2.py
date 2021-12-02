edc0147d8011b000a3dbe26ec980d11454f54462
Erlend Thorsrud <erlendthorsrud@gmail.com>
2021-12-02 20:50:15 +0100

    solved part 2

--- day2.py
+++ day2.py
@@ -1,4 +1,6 @@
 #!/bin/bash/python3
+
+### Part 1
 x = 0
 y = 0
 
@@ -15,4 +17,26 @@ with open('data/day2-data.txt') as file:
             case 'forward':
                 x += value
 
-print(x*y)
+print("Part 1: " + str(x*y))
+
+### Part 2
+
+x = 0
+y = 0
+aim = 0
+
+with open('data/day2-data.txt') as file: 
+    for line in file:
+        data = line.split(" ", 1)
+        command = data[0]
+        value = int(data[1])
+        match command: 
+            case 'up': 
+                aim -= value
+            case 'down':
+                aim += value
+            case 'forward':
+                x += value
+                y += aim * value
+
+print("Part 2: " + str(x*y))
