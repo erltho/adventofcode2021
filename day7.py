b5525f79c432077cdafd2d00243320230d383cee
Erlend Thorsrud <erlendthorsrud@gmail.com>
2021-12-11 11:15:56 +0100

    solved day 7

--- day7.py
+++ day7.py
@@ -1,16 +1,31 @@
 #!/bin/bash/python3
 
 import statistics as s
+import math
 
 with open("data/day7-data.txt", "r") as f:
     input = f.read() # Read all file in case values are not on a single line
-    print(input)
     raw_data = input.split(",")  # Convert strings to ints
 
 
-raw_data.sort()
 data = list(map(int, raw_data))
-print(raw_data)
-print(type(raw_data))
+data.sort()
 median = s.median(data)
-print(median)
+
+sum = 0
+for elem in data:
+    sum += int(abs(elem - median))
+
+
+
+### Part 2
+
+avg = s.mean(data)
+print(s.mean(data))
+sum = 0
+for elem in data:
+    diff = int(abs(elem - math.floor(avg)))
+    for i in range(diff):
+        sum += i + 1
+
+print(sum)
